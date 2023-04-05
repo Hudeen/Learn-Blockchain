ab59bcd0366f544b85f98f12263891fed415db0b
Huden <Vanyauskov2003@gmail.com>
2023-04-06 02:18:37 +0300

    Method works "approve"

--- src/utils/MintNftForTokens.tsx
+++ src/utils/MintNftForTokens.tsx
@@ -1,38 +1,41 @@
-import { contractAbi } from './ContractAbi'
 import { usePrepareContractWrite, useAccount, useContractWrite, useWaitForTransaction } from 'wagmi'
+import { contractAbi } from './ContractAbi'
+import { ethers } from 'ethers'
 import { Button, Box, Text } from '@chakra-ui/react'
 
-export default function MintNft() {
+export default function MintNftForTokens() {
+
+    const value = ethers.utils.parseEther("1.0")
 
     const { config, error: prepareError, isError: isPrepareError,
     } = usePrepareContractWrite({
         address: '0xC5aAc8d25B035Fc04f21dc85a62Cf6788C5bB3D4',
         abi: contractAbi,
-        functionName: 'mintEth',
+        functionName: 'mintTokens',
+        args: [value]
     })
 
     const { data, error, isError, write } = useContractWrite(config)
 
-    const { isSuccess } = useWaitForTransaction({ hash: data?.hash,})
-    
+    const { isSuccess } = useWaitForTransaction({ hash: data?.hash})
+
     const { address, isConnected } = useAccount()
 
     if (isConnected == true && address != undefined) return(
-
         <Box>
-            <Button color='#fff' bgColor='#000' onClick={() => write?.()}>Mint Eth</Button>
-            {isSuccess && (
-                <Box>
-                    Successfully minted your NFT!
-                </Box>
-            )}
-            {(isPrepareError || isError) && (
+          <Button color='#fff' bgColor='#000' onClick={() => write?.()}>Mint Token</Button>
+          {isSuccess && (
+            <Text color='#000'>
+                Successfully Mint Nft
+            </Text>
+          )}
+          {(isPrepareError || isError) && (
                 <Box color='#000'>Error: {(prepareError || error)?.message}</Box>
             )}
         </Box>
-    )
-    return(
-        <Text color='#000'>Hi</Text>
-    )
-}
+      )
+      return(
+        <Text color='#000'>Nothing</Text>
+      )
 
+}
--- src/web/Nft.tsx
+++ src/web/Nft.tsx
@@ -5,15 +5,17 @@ import {
   } from '@chakra-ui/react'
 import nft from '../assets/img/nft.png'
 import MintNft from '../utils/MintNft'
-import MintErc from'../utils/MintErc'
+import Approve from'../utils/Approve'
+import MintNftForTokens from '../utils/MintNftForTokens'
 
 export default function Nft(){
 return(
     <Flex gridRow='1' alignItems='flex-start' justifyContent='center' w='100%' h='85vh' bg='#f7fafc' color='#f7fafc'>
         <Center display='flex' flexDir='column' w='100%'>
             <Img src={nft} w='250px' borderRadius='10%' mt="50px" boxShadow='md' />
-            <MintNft />
-            <MintErc />
+                <MintNft />
+                <Approve />
+                <MintNftForTokens />
         </Center>
     </Flex>
 )
